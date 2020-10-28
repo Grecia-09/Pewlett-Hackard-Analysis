5c1d6a059e411fc60eba1974bfc9bc65a59d041b
Grecia Recinos <insiktuser@Insikts-MacBook-Pro-4.local>
2020-10-28 12:55:57 -0700

    Adding EmployeeDB.png schema.sql queries.sql

--- schema.sql
+++ schema.sql
@@ -7,13 +7,13 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE employees (
-	emp_no VARCHAR NOT NULL,
-    birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	gender VARCHAR NOT NULL,
-	hire_date DATE,
-	PRIMARY KEY (emp_no)
+	 emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_manager (
@@ -36,8 +36,8 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -47,11 +47,9 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 
 CREATE TABLE titles (
   emp_no INT NOT NULL,
-  title INT NOT NULL,
+  title VARCHAR(40) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  PRIMARY KEY (emp_no, title, from_date)
 );
-
-SELECT * FROM departments;
